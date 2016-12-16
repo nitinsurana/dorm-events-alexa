6d08252c3a6774f1068205761e8436ef893cad61
Nitin Surana <surana.nitin1990@gmail.com>
2016-12-16 01:10:56 -0800

    1. Added searchIntent with some functoinality and eventIntent (functionality pending). Updated sampleUtterances & intentSchema accordingly. 2. Code cleanup

--- usc_expert/src/index.js
+++ usc_expert/src/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-
 var http = require('http')
     , AlexaSkill = require('./AlexaSkill')
-    , APP_ID = 'amzn1.ask.skill.789b89ef-790a-4002-b17b-5da0e3036248';
-var ical = require('ical');
-var request = require('request');
+    , APP_ID = 'amzn1.ask.skill.789b89ef-790a-4002-b17b-5da0e3036248'
+    , ical = require('ical')
+    , moment = require('moment')
+    , request = require('request')
+    , fbManager = require('./fb_manager');
+
+var dateOutOfRange = "Date is out of range please choose another date";
 
 var MyObject = function () {
     AlexaSkill.call(this, APP_ID);
@@ -26,11 +29,6 @@ MyObject.prototype.intentHandlers = {
     ColloquiumToday: function (intent, session, response) {
         handleColloquiumToday(intent, session, response);
     },
-    InterestedColloquiumToday: function (intent, session, response) {
-        handleInterestedColloquiumToday(intent, session, response);
-    },
-
-
     HelpIntent: function (intent, session, response) {
         // var speechOutput = 'You can ask me if there are colloquiums today or about events your friends are interested in.' +
         //     'For example, you can say, Are there any colloquiums today.';
@@ -54,103 +52,50 @@ MyObject.prototype.intentHandlers = {
             //     // this.emit(':tell', noAccessToken, tryLaterText);
         }
     },
+    searchIntent: function (intent, session, response) {
+        var slotValue = intent.slots.date.value;
+        slotValue = getDateFromSlot(slotValue);
+        var accessToken = session.user.accessToken;
+        var fb = new fbManager(accessToken);
 
-    MyUpcomingFacebookEventsIntent: function (intent, session, response) {
-        handleMyUpcomingFacebookEventsIntent(intent, session, response);
-    }
-};
-
-var events = [
-    {
-        type: 'colloquium',
-        title: 'Representations and Models for Collaboratively Intelligent Robots',
-        presenter: 'Subramanian Ramamoorthy',
-        location: 'SAL one zero one',
-        date: 'today',
-        time: "three thirty PM",
-        friends: [
-            {
-                id: '',
-                name: ''
-            }
-        ]
-    },
-    {
-        type: 'colloquium',
-        title: 'Learning with Low Samples in High-Dimensions: Estimators, Geometry, and Applications',
-        presenter: 'Arindam Banerjee',
-        location: 'SAL one zero one',
-        date: 'today',
-        time: "four PM",
-        friends: [
-            {
-                id: '',
-                name: ''
-            },
-            {
-                id: '',
-                name: ''
-            },
-            {
-                id: '',
-                name: ''
-            }
-        ]
-    }
-];
-
-function handleMyUpcomingFacebookEventsIntent(intent, session, response) {
-    var eventList = [], output;
-    var URL = "http://www.facebook.com/ical/u.php?uid=1659106026&key=AQAg7TvEGtIJVguR";
-    // ical.parseICS
-    request({
-        url: URL,
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Node.js 6.0.0) MagicMirror/2.0 (https://github.com/MichMich/MagicMirror)'
-        }
-    }, function (error, res, data) {
-        // console.log(icalData);
-        // });
-        // ical.fromURL(URL, {}, function (err, data) {
-        // Loop through all iCal data found
-        // console.log("Ical data before parsing");
-        // console.log(data);
-        // console.log(data);
-        data = ical.parseICS(data);
-        console.log(data);
-        for (var k in data) {
-            if (data.hasOwnProperty(k)) {
-                var ev = data[k];
-                // Pick out the data relevant to us and create an object to hold it.
-                var eventData = {
-                    summary: removeTags(ev.summary),
-                    location: removeTags(ev.location),
-                    description: removeTags(ev.description),
-                    start: ev.start,
-                    created: ev.created
-                };
-                // add the newly created object to an array for use later.
-                eventList.push(eventData);
-            }
-        }
-        var index = 0;
-
-        // Check we have data
-        if (eventList.length > 0) {
-            var today = 20161116;
-            eventList.forEach(function (e, i) {
-                var created = parseInt(e.created.split("T")[0]);
-                if (created > today) {
-                    index = i;
+        if (slotValue.error) {
+            response.tell(slotValue.error);
+        } else {
+            console.log('Searching for ' + intent.slots.date.value + "    " + slotValue.startDate + '     ' + slotValue.endDate);
+            var p = fb.getAllEvents();
+            p.then(function (arr) {
+                var events = fb.findEvents(slotValue.startDate, slotValue.endDate);
+                console.log(events);
+                if (events && events.length) {
+                    response.tell("Total " + events.length + " events found. First one is " + events[0].name);
+                } else {
+                    response.tell("Sorry, no events found");
                 }
             });
-            output = eventList[index].summary + " in " + eventList[index].location;
-        } else {
-            output = "There are no upcoming events";
         }
-        response.tell(output);
-    });
-}
+
+    },
+    eventIntent: function (intent, session, response) {
+
+        var repromt = " Would you like to hear another event?";
+        var slotValue = intent.slots.number.value;
+
+        // parse slot value
+        var index = parseInt(slotValue) - 1;
+
+        //     if (relevantEvents[index]) {
+        //
+        //         // use the slot value as an index to retrieve description from our relevant array
+        //         var output = descriptionMessage + removeTags(relevantEvents[index].description);
+        //
+        //         output += repromt;
+        //
+        //         this.emit(':askWithCard', output, repromt, relevantEvents[index].summary, output);
+        //     } else {
+        //         this.emit(':tell', eventOutOfRange);
+        //     }
+    }
+};
 
 function removeTags(str) {
     return str && str.replace(/<(?:.|\n)*?>/gm, '');
@@ -173,20 +118,90 @@ function handleColloquiumToday(intent, session, response) {
     }
 }
 
-function handleInterestedColloquiumToday(intent, session, response) {
-    var maxFriends = 0, output;
-    events.forEach(function (e) {
-        if (e.date === 'today' && e.friends && e.friends.length && maxFriends < e.friends.length) {
-            maxFriends = e.friends.length;
-            output = maxFriends + ' of your friends are going to ' + (e.title + ' by ' + e.presenter + ' at ' + e.time + ' in ' + e.location);
+// Given an AMAZON.DATE slot value parse out to usable JavaScript Date object
+// Utterances that map to the weekend for a specific week (such as �this weekend�) convert to a date indicating the week number and weekend: 2015-W49-WE.
+// Utterances that map to a month, but not a specific day (such as �next month�, or �December�) convert to a date with just the year and month: 2015-12.
+// Utterances that map to a year (such as �next year�) convert to a date containing just the year: 2016.
+// Utterances that map to a decade convert to a date indicating the decade: 201X.
+// Utterances that map to a season (such as �next winter�) convert to a date with the year and a season indicator: winter: WI, spring: SP, summer: SU, fall: FA)
+function getDateFromSlot(rawDate) {
+    // try to parse data
+    var date = new Date(Date.parse(rawDate));
+    var result;
+    // create an empty object to use later
+    var eventDate = {};
+
+    // if could not parse data must be one of the other formats
+    if (isNaN(date)) {
+        // to find out what type of date this is, we can split it and count how many parts we have see comments above.
+        var res = rawDate.split("-");
+        // if we have 2 bits that include a 'W' week number
+        if (res.length === 2 && res[1].indexOf('W') > -1) {
+            var dates = getWeekData(res);
+            eventDate["startDate"] = new Date(dates.startDate);
+            eventDate["endDate"] = new Date(dates.endDate);
+            // if we have 3 bits, we could either have a valid date (which would have parsed already) or a weekend
+        } else if (res.length === 3) {
+            var dates = getWeekendData(res);
+            eventDate["startDate"] = new Date(dates.startDate);
+            eventDate["endDate"] = new Date(dates.endDate);
+            // anything else would be out of range for this skill
+        } else {
+            eventDate["error"] = dateOutOfRange;
         }
-    });
-    if (!output) {
-        response.tell("There are no colloquiums today");
+        // original slot value was parsed correctly
     } else {
-        response.tell(output);
+        eventDate["startDate"] = new Date(date.setUTCHours(0, 0, 0, 0));
+        eventDate["endDate"] = new Date(date.setUTCHours(24, 0, 0, 0));
+    }
+    return eventDate;
+}
+
+// Given a week number return the dates for both weekend days
+function getWeekendData(res) {
+    if (res.length === 3) {
+        var saturdayIndex = 5;
+        var sundayIndex = 6;
+        var weekNumber = res[1].substring(1);
+
+        var weekStart = w2date(res[0], weekNumber, saturdayIndex);
+        var weekEnd = w2date(res[0], weekNumber, sundayIndex);
+
+        return {
+            startDate: weekStart,
+            endDate: weekEnd
+        };
     }
 }
+
+// Given a week number return the dates for both the start date and the end date
+function getWeekData(res) {
+    if (res.length === 2) {
+
+        var mondayIndex = 0;
+        var sundayIndex = 6;
+
+        var weekNumber = res[1].substring(1);
+
+        var weekStart = w2date(res[0], weekNumber, mondayIndex);
+        var weekEnd = w2date(res[0], weekNumber, sundayIndex);
+
+        return {
+            startDate: weekStart,
+            endDate: weekEnd
+        };
+    }
+}
+
+// Used to work out the dates given week numbers
+var w2date = function (year, wn, dayNb) {
+    var day = 86400000;
+
+    var j10 = new Date(year, 0, 10, 12, 0, 0),
+        j4 = new Date(year, 0, 4, 12, 0, 0),
+        mon1 = j4.getTime() - j10.getDay() * day;
+    return new Date(mon1 + ((wn - 1) * 7 + dayNb) * day);
+};
 
 exports.handler = function (event, context, callback) {
     var skill = new MyObject();
--- usc_expert/src/test.js
+++ usc_expert/src/test.js
@@ -1,12 +1,12 @@
-var fbManager = require('./fb_manager');
-
-var fb = new fbManager("EAAJZAbxXrAugBAHFDJqiB6NducoAPUNNflV8l51n2Gcg8oYrp0bJPC93rwMXLTOz0sOyqcQXXkynSLcdq1W0H8NjgfducvRs96EZAp1xv4rtVLCR3za9JonUBhUpZBx4Vve9qo60lN9bFwCX5STwtY4ZAMFqqdUZD");
-
-var p = fb.getAllEvents();
-p.then(function (arr) {
-    // console.log(arr);
-
-    var events = fb.findEvents(new Date(), new Date(new Date().getTime() + 1000 * 45 * 24 * 3600));
-    console.log(events);
-});
-
+// var fbManager = require('./fb_manager');
+//
+// var fb = new fbManager("EAAJZAbxXrAugBAHFDJqiB6NducoAPUNNflV8l51n2Gcg8oYrp0bJPC93rwMXLTOz0sOyqcQXXkynSLcdq1W0H8NjgfducvRs96EZAp1xv4rtVLCR3za9JonUBhUpZBx4Vve9qo60lN9bFwCX5STwtY4ZAMFqqdUZD");
+//
+// var p = fb.getAllEvents();
+// p.then(function (arr) {
+//     // console.log(arr);
+//
+//     var events = fb.findEvents(new Date(), new Date(new Date().getTime() + 1000 * 45 * 24 * 3600));
+//     console.log(events);
+// });
+//
